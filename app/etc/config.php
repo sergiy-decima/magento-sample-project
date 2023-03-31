752e7d47ba6463cdce2163aafb1da14302c3ffed
Yur <sergiy.yur@decimadigital.com>
2023-03-31 19:32:44 +0300

    Update magento-the-best-module module

--- app/etc/config.php
+++ app/etc/config.php
@@ -346,6 +346,7 @@ return [
         'Magento_WishlistAnalytics' => 1,
         'Magento_WishlistGraphQl' => 1,
         'Magento_WishlistSampleData' => 1,
+        'Decima_Core' => 1,
         'Decima_TheBestModule' => 1,
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeGraphQl' => 1,
